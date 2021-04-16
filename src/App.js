6d53b026d3a566afa7cf26d37679518806f5556d
Nil Golan <Nil9711@icloud.com>
2021-04-16 17:53:48 +0300

    lecture 185

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
-import './App.css';
-import Dogs from './Dogs/Dogs'
-import Dog from './Dog/Dog'
-import { Route, Switch } from "react-router-dom";
-import { Redirect } from "react-router-dom";
-import { NavLink } from "react-router-dom";
-import { getDogsList } from './DogsList'
+import React, { Component } from "react";
+import Palette from "./Palette/Palette";
+import seedColors from "./seedColor/seedColor";
+import { generatePalette } from "./colorHelpers";
 
-
-function App() {
-  return (
-    <div className="App">
-      <nav className='App-nav'>
-        <NavLink exact className='not-active-link' activeClassName='active-link' to='/dogs'>
-          Dogs
-                </NavLink>
-        <NavLink exact className='not-active-link' activeClassName='active-link' to='/dogs/kai'>
-          Kai
-                </NavLink>
-        <NavLink exact className='not-active-link' activeClassName='active-link' to='/dogs/whiskey'>
-          Whiskey
-                </NavLink>
-        <NavLink exact className='not-active-link' activeClassName='active-link' to='/dogs/hazel'>
-          Hazel
-                </NavLink>
-        <NavLink exact className='not-active-link' activeClassName='active-link' to='/dogs/tubby'>
-          Tubby
-        </NavLink>
-      </nav>
-
-      <Switch>
-        <Route exact path='/dogs' render={() => <Dogs dogList={getDogsList()} />} />
-        <Route exact path='/dogs/:dogName' render={routeProps => <Dog name={routeProps.match.params.dogName} />} />
-        <Route exact path='/' render={() => <Redirect to='/dogs' />} />
-
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  render() {
+    console.log();
+    return (
+      <div>
+        <Palette palette={generatePalette(seedColors[4])} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Palette/Palette.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import ColorBox from '../ColorBox.js/ColorBox';
+import NavBar from '../NavBar/NavBar';
+
+import './Palette.css'
+
+class Palette extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { level: 500, format: "hex" };
+        this.changeLevel = this.changeLevel.bind(this);
+        this.changeFormat = this.changeFormat.bind(this)
+    }
+    changeLevel(level) {
+        this.setState({ level });
+    }
+    changeFormat(val) {
+        this.setState({ format: val })
+    }
+    render() {
+        const { colors } = this.props.palette;
+        const { level, format } = this.state;
+        const colorBoxes = colors[level].map(color => (
+            <ColorBox background={color[format]} name={color.name} />
+        ));
+        return (
+            <div className='Palette'>
+                <NavBar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat} />
+                <div className='Palette-colors'>{colorBoxes}</div>
+            </div>
+        );
+    }
+}
+export default Palette;
